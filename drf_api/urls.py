315f530529a2c9ad8a1338198fa519b955ba4479
Molly Adamsson <adamssonmolly@gmail.com>
2023-03-30 11:41:27 +0000

    Added mor econtent

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,5 +1,4 @@
 """drf_api URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -18,6 +17,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
-    
+    path('', include('posts.urls')),
 ]
